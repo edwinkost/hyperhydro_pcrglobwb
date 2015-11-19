6739e38dbde4c38e965be6e7844a8f146f5810f2
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-19 17:00:37 +0100

    Add the scripts for analyzing daily discharge.

--- daily_discharge_analysis_scripts/parallel_discharge_analysis.py
+++ daily_discharge_analysis_scripts/parallel_discharge_analysis.py
@@ -8,7 +8,7 @@ output_folder = "/projects/0/wtrcycle/users/edwin/edwin/hyperhydro_november_2015
 
 # starting and last run codes
 start_code = 0
-end_code = 624
+end_code = 10
 
 # initializing the counter
 i_code = start_code
@@ -20,7 +20,7 @@ while start_loop < (end_code + 1)
    
    # starting and end codes for this loop
    start_loop = i_code
-   end_loop   = max(start_loop, i_code + 24 -1)
+   end_loop   = max(start_loop, i_code + 2 -1)
    end_loop   = min(end_loop, end_code)
    
    # generating jobs for this loop
