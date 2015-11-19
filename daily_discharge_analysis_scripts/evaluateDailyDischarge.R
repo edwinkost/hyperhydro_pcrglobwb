4f3c856ed74e6b4d7a37dd3b63a1726ff1244626
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-19 14:05:03 +0100

    Add the scripts for analyzing daily discharge.

--- daily_discharge_analysis_scripts/evaluateDailyDischarge.R
+++ daily_discharge_analysis_scripts/evaluateDailyDischarge.R
@@ -285,7 +285,7 @@ outplott <- outplott +
  geom_text(aes(x = x_info_text, y = 0.00*y_max, label = paste(" correlation = ",round(correlation,2),sep="")), size = 2.5,hjust = 0) +
 #~ #
  scale_y_continuous("discharge",limits=c(y_min,y_max)) +
- scale_x_date('',limits=c(x_min,x_max)) +
+ scale_x_date_time('',limits=c(x_min,x_max)) +
  theme(legend.position = "none") 
 #ggsave("screen.pdf", plot = outplott,width=30,height=8.25,units='cm')
  ggsave(paste(outputFile,".pdf",sep=""), plot = outplott,width=27,height=7,units='cm')
