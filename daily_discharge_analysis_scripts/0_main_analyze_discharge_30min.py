95719f25c1d62dfb15abc13306929af8a96584b9
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-11-19 19:55:52 +0100

    Analysis for the years 2008-2013.

--- daily_discharge_analysis_scripts/0_main_analyze_discharge_30min.py
+++ daily_discharge_analysis_scripts/0_main_analyze_discharge_30min.py
@@ -35,8 +35,8 @@ except:
     pass 
 
 # time range for analyses
-startDate = None # "2003-01-01" # None # "1960-01-31" #YYYY-MM-DD # None 
-endDate   = None # "2003-12-31" # None # "2010-12-31" #YYYY-MM-DD # None 
+startDate = "2008-01-01" # None # "1960-01-31" #YYYY-MM-DD # None 
+endDate   = "2013-12-31" # None # "2010-12-31" #YYYY-MM-DD # None 
 
 # directory for GRDC files:
 globalDirectoryGRDC = "/home/edwin/github/edwinkost/hyperhydro_pcrglobwb/daily_discharge_analysis_scripts/rhine_daily_discharge_data/"
@@ -57,7 +57,7 @@ def main():
     ####################################################################################################
     #
     # make analysisOutputDir
-    analysisOutputDir = globalAnalysisOutputDir+"/daily_discharge/"
+    analysisOutputDir = globalAnalysisOutputDir+"/daily_discharge_2008-2013/"
     try:
         os.makedirs(analysisOutputDir) 
     except:
